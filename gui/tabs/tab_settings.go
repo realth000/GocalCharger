64c5d27a8a2ae2b01ddd9aab37e32f8b155859a9
realth000 <a598919335@gmail.com>
2022-06-04 21:55:11 +0800

    feat(Gui): Add settings apply button
    Add settings apply button:
    * Add apply button in settings page for server and client.

--- gui/tabs/tab_settings.go
+++ gui/tabs/tab_settings.go
@@ -1,19 +1,40 @@
 package tabs
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func makeServerSettingsArea() fyne.CanvasObject {
-	return widget.NewCard("Server", "Server settings", nil)
+	configFileLabel := widget.NewLabel("Config file path")
+	configFileEntry := widget.NewEntry()
+	configFileEntry.SetPlaceHolder("*.toml")
+	formLayout := container.New(layout.NewFormLayout(), configFileLabel, configFileEntry)
+	loadConfigFileButton := widget.NewButton("Load config file", reloadServerConfigFile)
+	buttonBox := container.NewHBox(container.NewVBox(loadConfigFileButton))
+	return widget.NewCard("Server", "Server settings", container.NewVBox(formLayout, buttonBox))
 }
 
 func makeClientSettingsArea() fyne.CanvasObject {
-	return widget.NewCard("Client", "Client settings", nil)
+	configFileLabel := widget.NewLabel("Config file path")
+	configFileEntry := widget.NewEntry()
+	configFileEntry.SetPlaceHolder("*.toml")
+	formLayout := container.New(layout.NewFormLayout(), configFileLabel, configFileEntry)
+	loadConfigFileButton := widget.NewButton("Load config file", reloadClientConfigFile)
+	buttonBox := container.NewHBox(container.NewVBox(loadConfigFileButton))
+	return widget.NewCard("Client", "Client settings", container.NewVBox(formLayout, buttonBox))
 }
 
+func reloadServerConfigFile() {
+	fmt.Println("reload server config file")
+}
+
+func reloadClientConfigFile() {
+	fmt.Println("reload server config file")
+}
 func NewSettingsTab() *container.TabItem {
 	serverSettings := makeServerSettingsArea()
 	clientSettings := makeClientSettingsArea()
