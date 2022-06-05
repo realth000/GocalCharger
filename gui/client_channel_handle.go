189fb34553b7aceac54683596d163b1066790649
realth000 <a598919335@gmail.com>
2022-06-05 21:45:08 +0800

    refactor(Gui): Channel handling sperate into files
    Channel handling sperate into files:
    * Add server_channel_handle.go.
    * Rename client_callback_handle.go to client_channel_handle.go.

--- gui/client_channel_handle.go
+++ gui/client_channel_handle.go
@@ -6,7 +6,6 @@ import (
 	"fyne.io/fyne/v2/dialog"
 	"gocalcharger/gui/action"
 	"gocalcharger/gui/tabs"
-	"log"
 	"strings"
 )
 
@@ -22,20 +21,5 @@ func handleClientSayHelloFailed(callback action.ClientSayHelloCallbackArgs) {
 	wrapString := strings.ReplaceAll(callback.Error.Error(), "error:", "error:\n")
 	errString := fmt.Sprintf("%s\n[Server: %s]", wrapString, callback.ServerTarget)
 	dialog.ShowError(errors.New(errString), mainWindow)
-	tabs.UpdateServerStatus(tabs.ServerClosed)
-}
-
-func handleServerStartGRPCSuccess(callback action.ServerStartGRPCArgs) {
-	log.Printf("Start gRPC server successful[ServeTarget=%s]", callback.ServeTarget)
-	tabs.UpdateServerStatus(tabs.ServerStarted)
-}
-
-func handleServerStartGRPCFailed(callback action.ServerStartGRPCArgs) {
-	wrapString := strings.ReplaceAll(callback.Error.Error(), "error:", "error:\n")
-	errString := fmt.Sprintf("%s\n[ServeTarget=%s]", wrapString, callback.ServeTarget)
-	dialog.ShowError(errors.New(errString), mainWindow)
-}
-
-func handleServerStopGRPCSuccess() {
 	tabs.UpdateServerStatus(tabs.ServerClosed)
 }
