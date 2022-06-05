46528f8d3d047b44d04feb68d7482ab8a5c83c10
realth000 <a598919335@gmail.com>
2022-06-06 01:00:12 +0800

    revert(Gui): Revert changing running goroutine.
    Revert changing running goroutine.

--- gui/ui_channel.go
+++ gui/ui_channel.go
@@ -16,53 +16,37 @@ var (
 )
 
 func StartReceivingChannels() {
-	go func() {
-		for {
-			select {
-			case x := <-*serverCallbackChannel:
-				switch x.CallbackName {
-				case action.ServerStartGRPCSuccess:
-					go handleServerStartGRPCSuccess(x.CallbackArgs.(action.ServerStartGRPCArgs))
-				case action.ServerStartGRPCFailed:
-					go handleServerStartGRPCFailed(x.CallbackArgs.(action.ServerStartGRPCArgs))
-				case action.ServerStopGRPCSuccess:
-					go handleServerStopGRPCSuccess()
-				}
+	for {
+		select {
+		case x := <-*clientCallbackChannel:
+			switch x.CallbackName {
+			case action.ClientSayHelloSuccess:
+				go handleClientSayHelloSuccess(x.CallbackArgs.(action.ClientSayHelloCallbackArgs))
+			case action.ClientSayHelloFailed:
+				go handleClientSayHelloFailed(x.CallbackArgs.(action.ClientSayHelloCallbackArgs))
+			case action.ClientDownloadUpdate:
+				go handleClientDownloadUpdate(x.CallbackArgs.(action.ClientDownloadUpdateArgs))
+			}
+		case x := <-*serverCallbackChannel:
+			switch x.CallbackName {
+			case action.ServerStartGRPCSuccess:
+				go handleServerStartGRPCSuccess(x.CallbackArgs.(action.ServerStartGRPCArgs))
+			case action.ServerStartGRPCFailed:
+				go handleServerStartGRPCFailed(x.CallbackArgs.(action.ServerStartGRPCArgs))
+			case action.ServerStopGRPCSuccess:
+				go handleServerStopGRPCSuccess()
+			}
+		case x := <-*uiTabsChannel:
+			switch x.ActionName {
+			case action.UIStartServer:
+				go server.StartServer()
+			case action.UIStopServer:
+				go server.StopServer()
+			case action.ClientSayHello:
+				go handleClientSayHello(x.ActionArgs.(action.ClientSayHelloArgs))
+			case action.UIDownloadFile:
+				go handleClientDownloadFile(x.ActionArgs.(action.UIDownloadFileArgs))
 			}
 		}
-	}()
-
-	go func() {
-		for {
-			select {
-			case x := <-*clientCallbackChannel:
-				switch x.CallbackName {
-				case action.ClientSayHelloSuccess:
-					go handleClientSayHelloSuccess(x.CallbackArgs.(action.ClientSayHelloCallbackArgs))
-				case action.ClientSayHelloFailed:
-					go handleClientSayHelloFailed(x.CallbackArgs.(action.ClientSayHelloCallbackArgs))
-				case action.ClientDownloadUpdate:
-					go handleClientDownloadUpdate(x.CallbackArgs.(action.ClientDownloadUpdateArgs))
-				}
-			}
-		}
-	}()
-
-	go func() {
-		for {
-			select {
-			case x := <-*uiTabsChannel:
-				switch x.ActionName {
-				case action.UIStartServer:
-					go server.StartServer()
-				case action.UIStopServer:
-					go server.StopServer()
-				case action.ClientSayHello:
-					go handleClientSayHello(x.ActionArgs.(action.ClientSayHelloArgs))
-				case action.UIDownloadFile:
-					go handleClientDownloadFile(x.ActionArgs.(action.UIDownloadFileArgs))
-				}
-			}
-		}
-	}()
+	}
 }
